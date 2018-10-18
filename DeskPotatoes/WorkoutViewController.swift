36953421271ee47d1c02c645b3d2d35e8969a495
Siyuan Chen <siyuanchen@siyuans-macbook-pro.local>
2018-10-18 16:21:28 -0400

    Add ViewController.swift for each added view controller

---
+++ DeskPotatoes/WorkoutViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WorkoutViewController.swift
+//  DeskPotatoes
+//
+//  Created by Siyuan Chen on 10/18/18.
+//  Copyright © 2018 Darren Powers. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
