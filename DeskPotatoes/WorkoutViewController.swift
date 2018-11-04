90583c206df7a877fbe82fc0ddb0afb4ea8bd168
Siyuan Chen <siyuanchen@siyuans-macbook-pro.local>
2018-11-03 22:27:09 -0400

    Add Sign in and Sign Up, exercise type button

--- DeskPotatoes/WorkoutViewController.swift
+++ DeskPotatoes/WorkoutViewController.swift
@@ -6,10 +6,30 @@
 //  Copyright © 2018 Darren Powers. All rights reserved.
 //
 
+import HealthKit
 import UIKit
+import CoreLocation
 
-class WorkoutViewController: UIViewController {
-
+class WorkoutViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var distance: UILabel!
+    @IBOutlet weak var mph: UILabel!
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var calorie: UILabel!
+    
+    let WALKING = 0; // User clicks Walking Button
+    let RUNNING = 1; // User clicks Running Button
+    let RIDING = 2;  // User clicks Riding Button
+    var selectedMode = 0; // Records which button is clicked
+    
+    var zeroTime = TimeInterval()
+    var timer : Timer = Timer()
+    
+    // Distance parts
+    let locationManager = CLLocationManager()
+    var startLocation: CLLocation!
+    var lastLocation: CLLocation!
+    var distanceTraveled = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
