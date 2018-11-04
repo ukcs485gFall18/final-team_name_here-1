1d68ad4989e4c8d1c0c5b9193cd6e7c101b9d044
Darren Powers <acmepowers@aol.com>
2018-11-04 11:00:41 -0500

    Minor additions and changes to HealthKit Manger between 10/23 and 11/1.

--- DeskPotatoes/Managers/HealthKitManager.swift
+++ DeskPotatoes/Managers/HealthKitManager.swift
@@ -15,11 +15,19 @@ import Foundation
 import HealthKit
 import UIKit
 
+/* TO DO: make sure to make toggleable parameter in settings for whether or not the share buttons will store data to healthkit */
+
 class HealthKitManager {
+    // Create an Enum of data for healthkit types
+    enum dataType {
+        case walkData
+        case activeEnergyBurned
+    }
+    
     // Create a Health Kit Store
     let healthKitStore: HKHealthStore = HKHealthStore()
     
-    func authorizeHealthKit(completion:( (_ success: Bool, _ error: NSError?) -> Void )!){
+    func authorizeHealthKit(authorizeType:dataType, completion:( (_ success: Bool, _ error: NSError?) -> Void )!){
         /* Authorize Health Kit: Darren Powers
          * This function performs the work of authorizing access to HealthKit
          */
@@ -28,9 +36,9 @@ class HealthKitManager {
 /* TO DO: Be more specific with what data will be requested than just activity summary type, if we need more. */
         
         // Specify Health Data to Read
-        let healthDataToRead = Set(arrayLiteral: HKObjectType.activitySummaryType())
+        let healthDataToRead = Set([HKObjectType.activitySummaryType()])
         // Specify Health Data to Write
-        let healthDataToWrite = Set(arrayLiteral: HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceWalkingRunning)!)
+        let healthDataToWrite = Set([HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceWalkingRunning)!])
         // Verify HealthKit data is available
         if !HKHealthStore.isHealthDataAvailable() {
             print("Health Kit Data Unavailable on this Device")
@@ -51,6 +59,13 @@ class HealthKitManager {
          * and does the work of saving the data appropriately to HealthKit
          */
 /* TO DO: Code for Share Data */
+    }
+    
+    func readData(dataType: HKQuantityTypeIdentifier, target: dataType) {
+/* Read Data: Darren Powers
+ * This function takes a HealthKit QUantity Type Identifier and a Healthkit Object target as parameters
+ * and retrieves the specified information from HealthKit
+ */
     }
     
 }
