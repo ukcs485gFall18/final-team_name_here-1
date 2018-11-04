32e10064f03e6c58730efb1026d4d733a8fa59b9
Darren Powers <acmepowers@aol.com>
2018-11-04 11:58:53 -0500

    Modifications to HealthKit manager to apply generalizing of the functions used in midterm. includes storyboard and view controllers created by Siyuan Chen.

--- DeskPotatoes/Managers/HealthKitManager.swift
+++ DeskPotatoes/Managers/HealthKitManager.swift
@@ -15,37 +15,23 @@ import Foundation
 import HealthKit
 import UIKit
 
-/* TO DO: make sure to make toggleable parameter in settings for whether or not the share buttons will store data to healthkit */
-
 class HealthKitManager {
-    // Create an Enum of data for healthkit types
-    enum dataType {
-        case walkData
-        case activeEnergyBurned
-    }
     
     // Create a Health Kit Store
     let healthKitStore: HKHealthStore = HKHealthStore()
     
-    func authorizeHealthKit(authorizeType:dataType, completion:( (_ success: Bool, _ error: NSError?) -> Void )!){
+    func authorizeHealthKit(share: Set<HKSampleType>?, read: Set<HKObjectType>?, completion:( (_ success: Bool, _ error: NSError?) -> Void )!){
         /* Authorize Health Kit: Darren Powers
          * This function performs the work of authorizing access to HealthKit
          */
         
-/* TO DO: Make sure this code allows for users to approve/deny requests */
-/* TO DO: Be more specific with what data will be requested than just activity summary type, if we need more. */
-        
-        // Specify Health Data to Read
-        let healthDataToRead = Set([HKObjectType.activitySummaryType()])
-        // Specify Health Data to Write
-        let healthDataToWrite = Set([HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceWalkingRunning)!])
         // Verify HealthKit data is available
         if !HKHealthStore.isHealthDataAvailable() {
             print("Health Kit Data Unavailable on this Device")
             return
         }
         // Actually request the authorization from HealthKit
-        healthKitStore.requestAuthorization(toShare: healthDataToWrite, read: healthDataToRead) { success, error in
+        healthKitStore.requestAuthorization(toShare: share, read: read) { success, error in
             guard error == nil, success else {
                 print(error!)
                 return
@@ -61,11 +47,41 @@ class HealthKitManager {
 /* TO DO: Code for Share Data */
     }
     
-    func readData(dataType: HKQuantityTypeIdentifier, target: dataType) {
+    func readData(dataType: HKObjectType, completion:((HKObject?, NSError?) -> Void)!) {
 /* Read Data: Darren Powers
  * This function takes a HealthKit QUantity Type Identifier and a Healthkit Object target as parameters
  * and retrieves the specified information from HealthKit
  */
+        if let data = dataType as? HKSampleType { //The data coming in is sample-type data
+            // Build query predicate
+            let distantPast = NSDate.distantPast as NSDate
+            let currentDate = NSDate()
+            let lastPredicate = HKQuery.predicateForSamples(withStart: distantPast as Date, end: currentDate as Date, options: [])
+            
+            // Get the one most recent piece of data
+            let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
+            
+            // Query HealthKit for the last entry
+            let query = HKSampleQuery(sampleType: data, predicate: lastPredicate, limit: 1, sortDescriptors: [sortDescriptor]) {
+                (sampleQuery, results, error) -> Void in
+                
+                // If there is an error, do not return object, return error
+                if let queryError = error {
+                    completion?(nil, queryError as NSError)
+                    return
+                 }
+                
+                // Set the first Sample in results as the most recent one
+                let lastSample = results!.first
+                if completion != nil {
+                    completion(lastSample, nil) //Return the object, not an error
+                }
+            }
+            // Execute the query generated above
+            self.healthKitStore.execute(query)
+            return
+        } else {
+            print("query \(dataType) type not currently supported")
+        }
     }
-    
 }
