12d9165f9671c2d566982be67fcf4b26aae3e043
Darren Powers <dcpowe4@uky.edu>
2018-12-10 11:31:45 -0500

    Substantial changes to organization. Removing redundancies. repairing some broken navigation.

---
+++ DeskPotatoes/Models/HealthModel.swift
@@ -0,0 +1,35 @@
+//
+//  HealthModel.swift
+//  DeskPotatoes
+//
+//  Created by Darren Powers on 10/12/2018.
+//  Copyright © 2018 Darren Powers. All rights reserved.
+//
+
+
+/* HealthModel
+ * this handles work for prepping information to be displayed by the
+ * View Controllers
+ */
+
+import Foundation
+import UIKit
+import HealthKit
+
+class HealthModel {
+    let healthManager: HealthKitManager = HealthKitManager()
+    
+    func authorizeHomepageElements() {
+        let reader: Set = [HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height), HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.bodyMass), HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.activeEnergyBurned), HKObjectType.activitySummaryType()]
+        self.healthManager.authorizeHealthKit(share: nil, read: (reader as! Set<HKObjectType>)) { (auth, error) in
+            if ((error) != nil) {
+                print("unable to authorize: \(String(describing: error))")
+            } else {
+                print("healthkit authorized for homepage elements")
+            }
+        }
+    }
+    
+
+}
+
